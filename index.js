23f94add47324a0df9f9c13e81c79f36472aee0e
Maymoiz <Moishamayndlovu@gmail.com>
2025-07-30 16:08:08 +0200

    Added Javascript
    Injected moment.js to the project for time and date

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+function updateTime() {
+  let Joburg = document.querySelector("#jdate");
+  Joburg.innerHTML = moment().tz("Africa/Johannesburg").format("DD MMMM YYYY");
+  let JoburgTime = document.querySelector("#joburg .time");
+  JoburgTime.innerHTML = moment()
+    .tz("Africa/Johannesburg")
+    .format("hh:mm:ss[<small>]A[</small>]");
+  let Washington = document.querySelector("#ndate");
+  Washington.innerHTML = moment().tz("America/New_York").format("DD MMMM YYYY");
+  let WashingtonTime = document.querySelector("#new-york .time");
+  WashingtonTime.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss[<small>]A[</small>]");
+  let HongKong = document.querySelector("#hdate");
+  HongKong.innerHTML = moment().tz("Asia/Hong_Kong").format("DD MMMM YYYY");
+  let HongKongTime = document.querySelector("#hong-kong .time");
+  HongKongTime.innerHTML = moment()
+    .tz("Asia/Hong_Kong")
+    .format("hh:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
